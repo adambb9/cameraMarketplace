b60b08f8d00b3aad7497dfb55de7791b08832442
Adam Biedka Whitton <adambb914@gmail.com>
2022-02-21 15:59:56 +0100

    camera validations

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -1,3 +1,11 @@
 class Camera < ApplicationRecord
+  CATEGORIES = ["DSLR", "Video Camera Tape", "Video Camera HD", "Action Cams", "Drones", "Analog", "360 degrees", "Waterproof", "Instant", "Handy Cam" ]
+  CONDITIONS = ["brand new", "slightly used", "used", "slight cosmetic damage", "bad cosmetic damage", "in need of repair", "non-functional" ]
   belongs_to :user
+
+  vaidates :category, :make, :model, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :condition, inclusion: { in: CONDITIONS }
+  validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100_000 }
+  validates :year, numericality: { only_integer: true }
 end
