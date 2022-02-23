52a43b327d66ae03ddbbfabd0e8cc5d4d8708fc0
scott thomas <srwthomas1@gmail.com>
2022-02-23 15:38:09 +0100

    new seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :cameras, dependent: :destroy
-  has_many :send_bookings, class_name: "Booking"
+  has_many :send_bookings, class_name: "Booking", dependent: :destroy
   has_many :recieved_bookings, through: :cameras, source: :bookings
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,18 @@
 User.destroy_all
 
 Camera.destroy_all
+
+Booking.destroy_all
+
 puts "DB cleaned!"
 
 user1 = User.create!(first_name: "John", last_name: "Doe", email: "john@gmail.com", password: "Hello1")
-user2 = User.create!(first_name: "Hafthor", last_name: "Jonsson", email: "BJonsson@gmail.com", password: "Reykjavik")
+user2 = User.create!(first_name: "Hafthor", last_name: "Jonsson", email: "BJonsson@gmail.com", password: "Hello1")
+user3 = User.create!(first_name: "James", last_name: "Deo", email: "j@gmail.com", password: "Hello1")
 
-Camera.create!(category: "Video Camera HD", condition: "used", make: "Panasonic", model: "HVX", price: 750.00, year: 2003, user_id: user1.id)
+
+Camera.create!(category: "Video Camera HD", condition: "used", make: "Panasonic", model: "HVX", price: 850.00, year: 2003, user_id: user1.id)
 Camera.create!(category: "Mirrorless", make: "Panasonic", condition: "brand new", model: "Lumix G100", price: 600.00, year: 2020, user_id: user2.id)
+Camera.create!(category: "Video Camera Tape", condition: "slightly used", make: "Sony", model: "VX1000", price: 450.00, year: 2003, user_id: user3.id)
+
 puts "DB seeded!"
