810f79b367ef02410de51e4ee83ae81fdf542cb5
scott thomas <srwthomas1@gmail.com>
2022-08-18 12:12:24 +0200

    fixed navbar errors

---
+++ app/policies/accessory_policy.rb
@@ -0,0 +1,24 @@
+class AccessoryPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    return true
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_095419) do
+ActiveRecord::Schema.define(version: 2022_06_02_150101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accessories", force: :cascade do |t|
+    t.string "category"
+    t.string "make"
+    t.text "condition"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
