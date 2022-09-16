0d13fec51c1a2f14bb9d564f5b2253c62859f89b
scott thomas <srwthomas1@gmail.com>
2022-09-16 11:59:26 +0200

    accessories section added

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -5,15 +5,15 @@ class Accessory < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many_attached :photos
 
-  validates :category, :make, :model, presence: true
+  validates :category, :make, presence: true
   validates :category, inclusion: { in: CATEGORIES }
   validates :condition, inclusion: { in: CONDITIONS }
   validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100_000 }
-  validates :year, numericality: { only_integer: true }
+  # validates :year, numericality: { only_integer: true }
 
   include PgSearch::Model
-  pg_search_scope :search_by_category_make_model_price_and_condition,
-    against: [ :category, :make, :model, :price, :condition ],
+  pg_search_scope :search_by_category_make_price_and_condition,
+    against: [ :category, :make, :price, :condition ],
     using: {
       tsearch: { prefix: true }
     }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_150101) do
+ActiveRecord::Schema.define(version: 2022_09_16_092300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2022_06_02_150101) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_accessories_on_user_id"
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -91,6 +93,7 @@ ActiveRecord::Schema.define(version: 2022_06_02_150101) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accessories", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "cameras"
