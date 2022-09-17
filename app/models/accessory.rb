af3b7b80864f8e412122f80b9a971873ff56d561
scott thomas <srwthomas1@gmail.com>
2022-09-17 15:53:10 +0200

    added rough views pre model generate

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -1,5 +1,5 @@
 class Accessory < ApplicationRecord
-  CATEGORIES = ["Lights", "Tripod", "Memory Cards", "Adapters", "Stabilizers", "Charger", "Batteries","Bags", "Other" ]
+  CATEGORIES = ["Lights", "Tripods", "Memory Cards", "Adapters", "Stabilizers", "Chargers", "Batteries","Bags", "Other" ]
   CONDITIONS = ["brand new", "slightly used", "used", "slight cosmetic damage", "bad cosmetic damage", "in need of repair", "non-functional" ]
   belongs_to :user
   has_many :bookings, dependent: :destroy
@@ -9,7 +9,6 @@ class Accessory < ApplicationRecord
   validates :category, inclusion: { in: CATEGORIES }
   validates :condition, inclusion: { in: CONDITIONS }
   validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100_000 }
-  # validates :year, numericality: { only_integer: true }
 
   include PgSearch::Model
   pg_search_scope :search_by_category_make_price_and_condition,
