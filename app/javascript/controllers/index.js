8a18a70a8e19c258fc81512b3b97e61460e00cda
scott thomas <srwthomas1@gmail.com>
2023-03-29 23:33:19 +0200

    remove btns from filter - model list reorganize / update 4 tests

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,7 +5,6 @@ import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 // import { Carousel }from "stimulus-carousel"
 
-
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
